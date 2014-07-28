d41a3a1aee6cc3a8ed926b4a1e6cbb31951c08d8
Mike Dawson <mike@toughra.com>
2014-07-28 17:10:13 +0200

    Add check for device API level

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Mobile browsers are using restrictions to prevent data
 allowance wastage - that silently ignore .play() and autoplay
 on audio and video tags.
 
+Tested only against loading files over HTTP: results may vary
+when using app package assets.
+
 Support for:
 
 * Android only (at present)
@@ -12,3 +15,5 @@ Support for:
    https://code.google.com/p/chromium/issues/detail?id=178297
 
 
+
+ 
--- platforms/android/src/org/apache/cordova/CordovaMediaSanity.java
+++ platforms/android/src/org/apache/cordova/CordovaMediaSanity.java
@@ -16,8 +16,11 @@ public class CordovaMediaSanity {
 	public static void setMediaGestureRequired(final CordovaActivity activity, final boolean gestureRequired, final CallbackContext callbackContext) {
 		activity.runOnUiThread(new Runnable() {
             public void run() {
-            	WebSettings settings = activity.appView.getSettings();
-        		settings.setMediaPlaybackRequiresUserGesture(gestureRequired);
+                if(Build.VERSION.SDK_INT >= 17) {
+                	WebSettings settings = activity.appView.getSettings();
+            		settings.setMediaPlaybackRequiresUserGesture(gestureRequired);
+        		}
+        		//Versions less than API Version 17 do not require this
         		callbackContext.success();
             }
 		});
